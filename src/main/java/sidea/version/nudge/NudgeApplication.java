8d9d1fc32e5d61cd54bf10ba07fc133483141f70
DESKTOP-QJ5VISR\jksin <jksin13@naver.com>
2023-08-08 15:39:45 +0900

    init springboot back 230808

---
+++ src/main/java/sidea/version/nudge/NudgeApplication.java
@@ -0,0 +1,13 @@
+package sidea.version.nudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudgeApplication.class, args);
+	}
+
+}
